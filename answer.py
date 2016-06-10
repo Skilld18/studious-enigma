32ea17d549527091373587b015b5ef1763bd91d6
Russell Dunk <russelldunk@gmail.com>
2016-06-10 12:45:28 -0400

    Some work on get answers logic

--- answer.py
+++ answer.py
@@ -1,12 +1,45 @@
 #!/usr/bin/python3
 
+import utils
+
 class Answer:
+    key = 0
     questionKey = 0
-    testResults = []
+    userKey = 0
+    answer = ""
 
-    def getScore(user):
-        return 1
+    def __init__(self, key, questionKey, userKey, answer):
+        self.key = key
+        self.questionKey = questionKey
+        self.userKey = userKey
+        self.answer = answer
 
-    def getMaxScore(self):
-        return 1
+
+def get_all_answers():
+    answers = []
+    data = utils.sql('SELECT * FROM answers')
+    for row in data:
+        answers.append(Answer(*row))
+    return answers
+
+def get_user_answers(userKey):
+    results = []
+    for answer in get_all_answers():
+        if answer.userKey == userKey:
+            results.append(answer)
+    return results
+
+def get_question_answers(questionKey):
+    results = []
+    for answer in get_all_answers():
+        if answer.questionKey == questionKey:
+            results.append(answer)
+    return results
+
+def search(answerString):
+    results = []
+    for answer in get_all_answers():
+        if answerString.lower() in answer.answer.lower():
+            results.append(answer)
+    return results
 
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+import answer
 import question
 
-
 print(question.search('ipsum')[0].long)
+print(answer.search('ello')[0].answer)
+for answer in answer.get_all_answers():
+    print(answer.answer)
+
 
 
 
