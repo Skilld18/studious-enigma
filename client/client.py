71265dd256990eb479ccc765b38c0d8551f6e764
Russell <russelldunk@gmail.com>
2016-11-08 13:22:24 -0500

    Starting to change architecture to traditional client server

---
+++ client/client.py
@@ -0,0 +1,17 @@
+import socket
+
+# HOST, PORT = "159.203.6.218", 9999
+HOST, PORT = "localhost", 9999
+data = input()
+
+# Create a socket (SOCK_STREAM means a TCP socket)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    # Connect to server and send data
+    sock.connect((HOST, PORT))
+    sock.sendall(bytes(data + "\n", "utf-8"))
+
+    # Receive data from the server and shut down
+    received = str(sock.recv(1024), "utf-8")
+
+print("Sent:     {}".format(data))
+print("Received: {}".format(received))
